1921f6a5462498383ebec7df36dbd8ecedca6cc8
voletisailaja <voletisailaja59@gmail.com>
2022-04-11 14:03:17 +0530

    added files

---
+++ src/Cards/Cards.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react'
+import Card from './CardUI';
+
+class Cards extends Component{
+    render(){
+        return(
+
+        <div className="row">
+            <div className="col-lg-10">
+                <Card/>
+            </div>
+           
+
+        </div>
+        
+        
+        
+        );
+    }
+}
+
+export default Cards;
